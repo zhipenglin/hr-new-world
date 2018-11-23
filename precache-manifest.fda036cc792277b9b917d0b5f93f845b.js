2ea88d4ae38f9c5fa8b78491cbed8948a0133357
linzhipeng <zhipeng.lin@ifchange.com>
2018-11-23 21:14:39 +0800

    Updates

--- precache-manifest.fda036cc792277b9b917d0b5f93f845b.js
+++ precache-manifest.fda036cc792277b9b917d0b5f93f845b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/hr-new-world/static/js/runtime~main.72e63079.js"
   },
   {
-    "revision": "e592a30d2ac82da8d902",
-    "url": "/hr-new-world/static/js/main.e592a30d.chunk.js"
+    "revision": "c35d10e01c7efbdcd99a",
+    "url": "/hr-new-world/static/js/main.c35d10e0.chunk.js"
   },
   {
     "revision": "a657bbafc9226adf3bd2",
     "url": "/hr-new-world/static/js/1.a657bbaf.chunk.js"
   },
   {
-    "revision": "e592a30d2ac82da8d902",
-    "url": "/hr-new-world/static/css/main.d174b855.chunk.css"
+    "revision": "c35d10e01c7efbdcd99a",
+    "url": "/hr-new-world/static/css/main.48cb2283.chunk.css"
   },
   {
-    "revision": "db804b953f9447023c1cf2557b8754a3",
+    "revision": "cbe08aae6c6264f29d7f2ab92b5daec9",
     "url": "/hr-new-world/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hr-new-world/precache-manifest.aff94a9a23695424d3d6cec965370ff0.js"
+  "/hr-new-world/precache-manifest.fda036cc792277b9b917d0b5f93f845b.js"
 );
 
 workbox.clientsClaim();
